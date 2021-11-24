e457574a6bc7d0add089aa19e952f56f018e3ae4
UnrealKurd <git_user@mail.ru>
2021-11-24 22:12:19 +0300

    Начата разработка генерации

--- Traffic-Flow-Counter/Traffic-Flow-Counter.cpp
+++ Traffic-Flow-Counter/Traffic-Flow-Counter.cpp
@@ -1,10 +1,14 @@
 ﻿// Traffic-Flow-Counter.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-
+//Поколение - все вершины находящиеся на одном уровне (создано во избежание зацикливания генерируемой сети
 #include <iostream>
 const int nMaxConnects = 5; //Максимальное количесвто связей к и от вершины
-const int nMaxVertexes = 25; //Максимальное количество вершин
+const int nOneGenVertexes = 5; //Количество вершин в поколении
+const int nGenNumber = 7; //Максимальное количество поколений
+const int nMaxFlow = 10; //Максимальное значение пропускной способности
 struct Vertex;
+struct Edge;
 typedef Vertex* pVertex;
+typedef Edge* pEdge;
 struct Edge //Ребро
 {
     int nFlow; //Максимальный транспортный поток
@@ -16,12 +20,34 @@ struct Vertex //Вершина
     Edge aLast[nMaxConnects]; //Ребра идущие к вершине
     Edge aNext[nMaxConnects]; //Ребра идущие от вершины
 };
-Vertex CreateNetwork() //Создание сети
+Vertex aGenerations[nGenNumber][nOneGenVertexes];
+int nIdCounter;
+pVertex ConnectVertex(Vertex vCurrent, int nGenCounter) //Соединяем вершину с дргуими
 {
-    for (int i; i <= rand() % (nMaxConnects + 1); i++)
+    nIdCounter++;
+    if (nGenCounter <= nOneGenVertexes)
     {
+        vCurrent.nId = nIdCounter;
+        for (int i = 0; i <= rand() % nMaxConnects; i++)
+        {
+            vCurrent.aNext[i].nFlow = rand() % (nMaxFlow + 1);
+            vCurrent.aNext[i].pStart = &vCurrent;
+            for (int j = 0; j <= nOneGenVertexes; j++)
+                if (rand() % 2 == 1)
+                {
 
+                }
+            vCurrent.aNext[i].pEnd = &(aGenerations[nGenCounter][rand() % nOneGenVertexes]);
+            *(vCurrent.aNext[i].pEnd)->aLast = vCurrent
+
+        }        
     }
+    else
+        return nullptr;
+}
+void CreateNetwork() //Создание сети
+{
+   
 }
 int main()
 {
