b74cf9e80e277c4a4c768e1b25a2a20ece2542a0
UnrealKurd <git_user@mail.ru>
2021-12-09 14:27:13 +0300

    Initial Commit

--- Traffic-Flow-Counter/Traffic-Flow-Counter.cpp
+++ Traffic-Flow-Counter/Traffic-Flow-Counter.cpp
@@ -1,6 +1,7 @@
 ﻿// Traffic-Flow-Counter.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //Поколение - все вершины находящиеся на одном уровне (создано во избежание зацикливания генерируемой сети
 #include <iostream>
+#include "list.h"
 //#include <SFML/Graphics.hpp>
 const int nMaxConnects = 5; //Максимальное количесвто связей к и от вершины
 const int nOneGenNodes = 5; //Количество вершин в поколении
@@ -8,40 +9,45 @@ const int nGenNumber = 7; //Максимальное количество пок
 const int nMaxFlow = 10; //Максимальное значение пропускной способности
 struct Node;
 struct Edge;
-int nIdCounter = 1;
 
 struct Edge //Ребро
 {
     int nFlow; //Максимальный транспортный поток
-    int nEndId; //Id конечной вершины
+    int nFlowLeft; //Остаточный поток
+    int aEndId[2]; //Id конечной вершины
 };
 
 struct Node //Вершина
 {
-    int nId; //Индивидуальный номер вершины
+    int aId[2]; //Индивидуальный номер вершины
+    int nMark[2]; //Метка источника потока для алгоритма Форда-Фалкерсона
+    int nMarkFlow; //Метка приходящего потока для алгоритма Форда-Фалкерсона
+    int nFlow; //Поток приходящий на вершину в данную Итерацию алгоритма Форда-Фалкерсона
     Edge aNext[nMaxConnects]; //Ребра идущие от вершины
 };
 
-Node First; //Исток
-Node aNodes[nGenNumber][nOneGenNodes]; //Массив хранящий все вершины
-Node Last; //Исход
+Node aNodes[nGenNumber+2][nOneGenNodes]; //Массив хранящий все вершины
 
 void ConnectNode(Node &vCurrent, int nNextGen) //Соединяем вершину с дргуими
 {
-    if (nNextGen <= nGenNumber)
+    if (nNextGen <= nGenNumber + 1)
     {
     for (int i1 = 0; i1 <= nOneGenNodes - 1; i1++)
     {
         if (rand() % 2 == 1)
         {
-            if (aNodes[nNextGen - 1][i1].nId != 0)
+            if (aNodes[nNextGen - 1][i1].aId == 0)
             {
-                nIdCounter++;
-                aNodes[nNextGen - 1][i1].nId = nIdCounter;
+
+                aNodes[nNextGen - 1][i1].aId[0] = nNextGen;
+                aNodes[nNextGen - 1][i1].aId[1] = i1 + 1;
+
             }
 
-            vCurrent.aNext[i1].nEndId = aNodes[nNextGen - 1][i1].nId;
+            vCurrent.aNext[i1].aEndId[0] = aNodes[nNextGen - 1][i1].aId[0];
+            vCurrent.aNext[i1].aEndId[1] = aNodes[nNextGen - 1][i1].aId[1];
             vCurrent.aNext[i1].nFlow = 1 + rand() % nMaxFlow;
+            vCurrent.aNext[i1].nFlowLeft = vCurrent.aNext[i1].nFlow;
             ConnectNode(aNodes[nNextGen - 1][i1], nNextGen + 1);
         }
     }
@@ -50,15 +56,31 @@ void ConnectNode(Node &vCurrent, int nNextGen) //Соединяем вершин
 
 void CreateNetwork() //Создание сети
 {
-   First.nId = 1;
-   ConnectNode(First, 1);
-   nIdCounter++;
-   Last.nId = nIdCounter;
+   aNodes[0][0].aId[0] = 1;
+   aNodes[0][0].aId[1] = 1;
+   aNodes[0][0].nMark[0] = aNodes[0][0].aId[0];
+   aNodes[0][0].nMark[1] = aNodes[0][0].aId[1];
+   ConnectNode(aNodes[0][0], 2);
+   aNodes[nGenNumber+1][0].aId[0] = nGenNumber + 2;
+   aNodes[nGenNumber+1][0].aId[1] = 1;
+   for (int i1 = 0; i1 <= nOneGenNodes - 1; i1++)
+   {
+       if (aNodes[nGenNumber][i1].aId[0] != 0)
+       {
+            aNodes[nGenNumber][i1].aId[0] = aNodes[nGenNumber+1][0].aId[0];
+            aNodes[nGenNumber][i1].aId[1] = aNodes[nGenNumber+1][0].aId[1];
+       }
+   }
 }
 
 void drawNetwork() //Отрисовка сети
 {
     //    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+}
+
+void FordFalkersonAlgorithm()
+{
+
 }
 
 int main()
