a7ad4976504da0de35be18d9ca0644ecc5e04965
UnrealKurd <git_user@mail.ru>
2021-12-23 10:18:53 +0300

    Исправлены графические проблемы (теперь граф рисуется красиво)

--- Traffic-Flow-Counter/Traffic-Flow-Counter-GUI.cs
+++ Traffic-Flow-Counter/Traffic-Flow-Counter-GUI.cs
@@ -34,13 +34,13 @@ namespace Traffic_Flow_Counter
                 {
                     if (nodeNode.aId[0] != 0)
                     {
-                        graphics.DrawEllipse(pen, new Rectangle(nodeNode.aId[0]*width-10, nodeNode.aId[1]*height-10, 25, 25));
-                        graphics.DrawString(Convert.ToString(nodeNode.aId[0])+";"+Convert.ToString(nodeNode.aId[1]), new Font("Times New Roman", 12.0f), new SolidBrush(Color.Black), nodeNode.aId[0]*width-10, nodeNode.aId[1]*height-7);
+                        graphics.DrawEllipse(pen, new Rectangle(nodeNode.aId[0]*width-12, nodeNode.aId[1]*height-12, 24, 24));
+                        graphics.DrawString(Convert.ToString(nodeNode.aId[0])+";"+Convert.ToString(nodeNode.aId[1]), new Font("Times New Roman", 12.0f), new SolidBrush(Color.Black), nodeNode.aId[0]*width-12, nodeNode.aId[1]*height-9);
                         foreach (NetworkReader.Edge edgeEdge in nodeNode.aNext)
                         {
                             if (edgeEdge.aEndId != null)
                             {
-                                graphics.DrawLine(pen, nodeNode.aId[0] * width + 10, nodeNode.aId[1] * height, edgeEdge.aEndId[0] * width - 10, edgeEdge.aEndId[1] * height);
+                                graphics.DrawLine(pen, nodeNode.aId[0] * width + 12, nodeNode.aId[1] * height, edgeEdge.aEndId[0] * width - 12, edgeEdge.aEndId[1] * height);
                                 graphics.DrawString(Convert.ToString(edgeEdge.nFlow), new Font("Times New Roman", 9.0f), new SolidBrush(Color.Black), nodeNode.aId[0]*width + (edgeEdge.aEndId[0] - nodeNode.aId[0])*width/2 , nodeNode.aId[1]*height + (edgeEdge.aEndId[1] - nodeNode.aId[1])*height/2);
                             }
                         }
