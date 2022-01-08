e727afbb6807e441ace10487492e943643ef2990
mk_serdyuk <git_user@mail.ru>
2022-01-08 18:38:15 +0300

    Выбрана красивая конфигцрация вершин и ребер

--- Traffic-Flow-Counter/Traffic-Flow-Counter.cpp
+++ Traffic-Flow-Counter/Traffic-Flow-Counter.cpp
@@ -6,9 +6,9 @@
 #include <chrono>
 
 using namespace std;
-const int nMaxConnects = 5; //Максимальное количесвто связей к и от вершины
-const int nOneGenNodes = 5; //Количество вершин в поколении
-const int nGenNumber = 15; //Максимальное количество поколений
+const int nMaxConnects = 3; //Максимальное количесвто связей к и от вершины
+const int nOneGenNodes = 3; //Количество вершин в поколении
+const int nGenNumber = 3; //Максимальное количество поколений
 const int nMaxFlow = 20; //Максимальное значение пропускной способности
 const int nPossibility = 100; //Чем больше, тем больше шанс, что появится ребро
 struct Node;
